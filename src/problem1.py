7340033372d183ea492c8582af8afed6816d99ef
Drew Borman <bormanad@rose-hulman.edu>
2018-10-09 15:13:00 -0400

    Exam 2

--- src/problem1.py
+++ src/problem1.py
@@ -2,8 +2,8 @@
 Exam 2, problem 1.
 
 Authors: David Mutchler, Dave Fisher, Matt Boutell, their colleagues,
-         and PUT_YOUR_NAME_HERE.  October 2018.
-"""  # TODO: 1. PUT YOUR NAME IN THE ABOVE LINE.
+         and Drew Borman.  October 2018.
+"""  # DONE: 1. PUT YOUR NAME IN THE ABOVE LINE.
 
 import time
 import testing_helper
@@ -48,10 +48,10 @@ def main():
     print('Un-comment the calls in MAIN one by one')
     print(' to run the testing code as you complete the TODOs.')
 
-    # run_test_problem1a()
-    # run_test_problem1b()
-    # run_test_problem1c()
-    # run_test_problem1d()
+    run_test_problem1a()
+    run_test_problem1b()
+    run_test_problem1c()
+    run_test_problem1d()
 
 
 def run_test_problem1a():
@@ -183,6 +183,11 @@ def run_test_problem1a():
 
 
 def problem1a(strings):
+    list = []
+    for k in range(len(strings)):
+        list.append(len(strings[k]))
+
+    return list
     """
     What comes in:
       -- a sequence of strings
@@ -218,7 +223,7 @@ def problem1a(strings):
       :rtype: list of int
     """
     # -------------------------------------------------------------------------
-    # TODO: 2. Implement and test this function.
+    # DONE: 2. Implement and test this function.
     #          Tests have been written for you (above).
     # -------------------------------------------------------------------------
 
@@ -352,6 +357,12 @@ def run_test_problem1b():
 
 
 def problem1b(strings):
+    list = problem1a(strings)
+    count = 0
+    for k in range(len(strings)):
+        if is_prime(len(strings[k])):
+            count = count + 1
+    return count
     """
     What comes in:
       -- a sequence of strings
@@ -542,6 +553,7 @@ def run_test_problem1c():
 # (including possibly your own functions) appropriately, else NO credit for it.
 # -----------------------------------------------------------------------------
 def problem1c(strings):
+    return is_prime(problem1b(strings))
     """
     What comes in:
       -- a sequence of strings
@@ -736,6 +748,11 @@ def run_test_problem1d():
 
 
 def problem1d(strings):
+    for k in range(len(strings)):
+        if is_prime(len(strings[k])):
+            return strings[k]
+
+    return -1
     """
     What comes in:
       -- a sequence of strings
